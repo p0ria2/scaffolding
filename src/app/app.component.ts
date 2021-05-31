696b2215912953897c366dcdb71d58ee48c0819b
p0ria2 <poria.mast3r@gmail.com>
2021-06-01 01:25:41 +0430

    refactor(components/table): table component refactored to generic type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'scaffolding';
+  userType = User;
+  users: Partial<User>[] = [
+    {
+      firstName: 'سینا',
+      lastName: 'آقاجانی',
+      isAdmin: true
+    },
+    {
+      firstName: 'پوریا',
+      lastName: 'امن زاده',
+      isAdmin: false
+    }
+  ]
 }
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { getTableOptions } from 'src/app/decorators/table.decorator';
 import { User } from 'src/app/models/user.model';
 import { TableFieldOptions } from './../../decorators/table.decorator';
@@ -9,24 +9,14 @@ import { TableFieldOptions } from './../../decorators/table.decorator';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-  columns: TableFieldOptions<User>[] = [];
-  users: Partial<User>[] = [
-    {
-      firstName: 'سینا',
-      lastName: 'آقاجانی',
-      isAdmin: true
-    },
-    {
-      firstName: 'پوریا',
-      lastName: 'امن زاده',
-      isAdmin: false
-    }
-  ]
+  @Input() itemType;
+  @Input() items = []
+  columns: TableFieldOptions<any>[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    this.columns = getTableOptions(User);
+    this.columns = getTableOptions(this.itemType);
   }
 
 
